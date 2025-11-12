06bdf27b3f3b9e74bcf77016942082698827ca87
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-11-12 14:19:22 -0500

    update users.js

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   res.send('Users Page');
-});
+}); //Work from Quest 3
 
 //router.post('/:id',(req, res) =>(
 //  res.send('Getting User data: ${req.params.id'};
@@ -17,15 +17,18 @@ router.route("/:id").get(req, res) =>{ // work from class on 11/12/25
   res.send('Updating user with id: ${req.params.id'};
 });
 
+  const users = {(name:"George"), {name:"Justyna"});
+  router.param ("id", (req, res, next, id)=>{
+    
+  
 router.param("id", (req, res, next, id) =>{
   console.log('Accessing user #$(id)');
   next();
 });
   
-
 router.get('/new', (req, res) => {
-  res.send('Create a new user');
-});
+res.send('Create a new user');
+});  // Work from Quest 3
 
 
 module.exports = router;
