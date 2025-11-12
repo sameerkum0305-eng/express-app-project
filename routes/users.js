0ffeda697ee006c60639eb2ec7be06e15f338654
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-11-12 14:00:34 -0500

    added params

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,27 @@ router.get('/', (req, res) => {
   res.send('Users Page');
 });
 
+//router.post('/:id',(req, res) =>(
+//  res.send('Getting User data: ${req.params.id'};
+// });
+
+router.route("/:id").get(req, res) =>{ // work from class on 11/12/25
+  res.send('Getting User data: ${req.params.id'};
+}).delete((req, res) =>{
+  res.send('Deleting user with id: ${req.params.id'};
+}).put((req, res) =>{
+  res.send('Updating user with id: ${req.params.id'};
+});
+
+router.param("id", (req, res, next, id) =>{
+  console.log('Accessing user #$(id)');
+  next();
+});
+  
+
 router.get('/new', (req, res) => {
   res.send('Create a new user');
 });
+
 
 module.exports = router;
