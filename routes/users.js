af5c86e84733a3510569d67a36d407ca8d860f0d
sameerkum0305-eng <sameerkum0305@gmail.com>
2025-11-19 13:06:08 -0500

    Made sure all code is accurate from class

--- routes/users.js
+++ routes/users.js
@@ -1,34 +1,45 @@
-const express = require('express');
-const router = express.Router();
+const express = 
+require('express');
 
-router.get('/', (req, res) => {
-  res.send('Users Page');
-}); //Work from Quest 3
+const router = 
+express.Router(); 
 
-//router.post('/:id',(req, res) =>(
-//  res.send('Getting User data: ${req.params.id'};
+router.get('/', (req, res)=>{
+    res.render(`users/list`, {users:users})
+});
+router.get('/new', (req, res)=>{
+    res.send('New User Form');
+});
+router.post('/', (req, res)=>{
+    const firstName = req.body.firstName;
+    const isValid = firstName !=="";//Check if they are there, added
+    if(isValid)
+    {
+        console.log(`Adding User: ${firstName}`);
+        users.push({name:firstName});
+        console.log(`New Set of Users: ${users}`);
+        res.send("User Created!");
+    }
+    else{
+        console.log("Error adding user!");
+        res.render("users/new", {firstName:firstName});
+    }
+});
+// router.get('/:id',(req, res)=>{
+// 	res.send(`Getting User data: ${req.params.id}`);
 // });
-
-router.route("/:id").get(req, res) =>{ // work from class on 11/12/25
-  res.send('Getting User data: ${req.params.id'};
-}).delete((req, res) =>{
-  res.send('Deleting user with id: ${req.params.id'};
-}).put((req, res) =>{
-  res.send('Updating user with id: ${req.params.id'};
+router.route("/:id").get((req, res)=>{
+ 	res.send(`Getting User data: ${req.params.id}`);
+}).delete((req, res)=>{
+    res.send(`Deleting user with id: ${req.params.id}`);
+}).put((req, res)=>{
+    res.send(`Updating user with id: ${req.params.id}`);
 });
+const users =[{name:"George"},{name:"Justyna"}];
 
-  const users = {(name:"George"), {name:"Justyna"});
-  router.param ("id", (req, res, next, id)=>{
-    
-  
-router.param("id", (req, res, next, id) =>{
-  console.log('Accessing user #$(id)');
-  next();
+
+router.param("id", (req, res, next, id)=>{
+    console.log(`Accessing user #${id}`);
+    next();
 });
-  
-router.get('/new', (req, res) => {
-res.send('Create a new user');
-});  // Work from Quest 3
-
-
 module.exports = router;
